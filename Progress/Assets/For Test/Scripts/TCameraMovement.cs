e4c603967ff2bf3a88fdf0244a5236be2e47b121
MAXOFFX <qaqasneyivar@gmail.com>
2019-12-22 15:25:11 +0400

    restart vs test FPS player

---
+++ Progress/Assets/For Test/Scripts/TCameraMovement.cs
@@ -0,0 +1,56 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TCameraMovement : MonoBehaviour
+{
+
+    [Header("Settings")]
+    [SerializeField] private float sensitivity;
+    [SerializeField] private float clampX;
+
+    [Space]
+
+    [Header("Reference")]
+    [SerializeField] private Transform playerBody;
+
+    void Awake()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Start()
+    {
+        
+    }
+
+    private float mouseX;
+    private float mouseY;
+    private float xRotation;
+
+    void Update()
+    {
+        MouseRotateInputs();
+    }
+
+    void FixedUpdate()
+    {
+        MouseRotate();
+    }
+
+    void MouseRotate()
+    {
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -clampX, clampX);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+
+    void MouseRotateInputs()
+    {
+        mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+    }
+
+}
---
+++ Progress/Assets/For Test/Scripts/TPlayerMovement.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TPlayerMovement : MonoBehaviour
+{
+    
+    [Header("Settings")]
+    [SerializeField] private float speed;
+    [SerializeField] private float gravity = -9.81f;
+    [SerializeField] private float groundDistance = 0.4f;
+    [SerializeField] private float jumpHeight = 3f;
+    [SerializeField] private LayerMask groundMask;
+
+    [Space]
+
+    [Header("Reference")]
+    [SerializeField] private CharacterController controller;
+    [SerializeField] private Transform groundCheckTransform;
+
+    private bool isGrounded;
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MoveInputs();
+    }
+
+    void FixedUpdate()
+    {
+        Move();
+    }
+
+    float x;
+    float z;
+    Vector3 velocity;
+
+    void Move()
+    {
+        Vector3 move = transform.right * x + transform.forward * z;
+        velocity.y += gravity * Time.deltaTime;
+        isGrounded = Physics.CheckSphere(groundCheckTransform.position, groundDistance, groundMask);
+
+        controller.Move(velocity * Time.deltaTime);
+        controller.Move(move * speed * Time.deltaTime);
+    }
+
+    void MoveInputs()
+    {
+        x = Input.GetAxis("Horizontal");
+        z = Input.GetAxis("Vertical");
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -2f;
+        }
+
+        if(Input.GetButtonDown("Jump") && isGrounded)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * gravity);
+        }
+    }
+
+}
